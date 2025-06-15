488149cd5b1db478548aca66cd932ef532ed2f8a
Daniel Veiga <dvs.veiga78@gmail.com>
2025-06-15 17:07:55 -0300

    Tentando 4 ainda fazer o deploy

--- back-end/apiBack/connect.js
+++ back-end/apiBack/connect.js
@@ -12,13 +12,29 @@
 
 // console.log((await pool.query("SELECT * FROM clientes")).rows);
 
-import pg from "pg";
+// import pg from "pg";
 import "dotenv/config";
-const pool = new pg.Pool({
-  host: process.env.DB_HOST,
-  port: process.env.DB_PORT,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_DATABASE,
+// const pool = new pg.Pool({
+//   host: process.env.DB_HOST,
+//   port: process.env.DB_PORT,
+//   user: process.env.DB_USER,
+//   password: process.env.DB_PASSWORD,
+//   database: process.env.DB_DATABASE,
+// });
+// export default pool;
+
+// No arquivo connect do backend, cole o código abaixo:
+import pkg from "pg";
+const { Pool } = pkg;
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false,
+  },
 });
+
 export default pool;
