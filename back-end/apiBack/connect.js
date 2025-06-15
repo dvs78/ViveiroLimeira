31822d0143e88c10528dd4951d09963614518784
Daniel Veiga <dvs.veiga78@gmail.com>
2025-06-15 18:15:15 -0300

    Tentando 8 ainda fazer o deploy

--- back-end/apiBack/connect.js
+++ back-end/apiBack/connect.js
@@ -12,16 +12,18 @@
 
 import pg from "pg";
 
+const isProduction = process.env.NODE_ENV === "production";
+
 const pool = new pg.Pool({
-  connectionString: process.env.DATABASE_URL,
-  ssl: {
-    rejectUnauthorized: false,
-  },
+  connectionString:
+    process.env.DATABASE_URL ||
+    "postgres://postgres:Limao_10@localhost:5432/ViveiroLimeira",
+  ssl: isProduction ? { rejectUnauthorized: false } : false,
 });
 
 pool
   .connect()
   .then(() => console.log("✅ Conectado ao PostgreSQL"))
-  .catch((err) => console.error("❌ Falha ao conectar ao PostgreSQL:", err));
+  .catch((err) => console.error("❌ Erro ao conectar ao PostgreSQL:", err));
 
 export default pool;
--- back-end/crud/dbOnLine/DbClassOri.js
+++ back-end/crud/dbOnLine/DbClassOri.js
@@ -2,6 +2,17 @@ import pool from "../../apiBack/connect.js";
 
 class DbClass {
   // PEGAR TUDO
+  // async getAll(tabela, colunas) {
+  //   try {
+  //     const results = (
+  //       await pool.query(`SELECT ${colunas.join()} FROM ${tabela}`)
+  //     ).rows;
+  //     return results;
+  //   } catch (error) {
+  //     throw error;
+  //   }
+  // }
+
   async getAll(tableName, columnsArray) {
     try {
       const query = `SELECT ${columnsArray.join(", ")} FROM ${tableName}`;
@@ -16,24 +27,33 @@ class DbClass {
 
   // INSERIR
   async insertOne(tabela, columnsArray, valuesArray) {
-    const cliente = await pool.connect();
+    const cliente = await pool.connect(); // Criar uma conexão com o banco de dados
     try {
       let flagsArray = Array.from(new Array(columnsArray.length).keys()).map(
         (el) => `$${el + 1}`
       );
 
+      // const queryText = `INSERT INTO ${tabela} (${columnsArray.join()}) VALUES(${flagsArray.join()}) RETURNING *`;
+      // const result = await cliente.query(queryText, valuesArray);
+      // await cliente.query("COMMIT"); // Comitar, validar
+      // return result.rows[0]; // ✅ retorna o cliente inserido, incluindo o id
+
+      // const queryText = `INSERT INTO ${tabela} (${columnsArray.join()}) VALUES(${flagsArray.join()})`;
+      // await cliente.query("BEGIN TRANSACTION"); // Rodar a transaction
+      // await cliente.query(queryText, valuesArray); // Inserir
+      // await cliente.query("COMMIT"); // Comitar, validar
       const queryText = `INSERT INTO ${tabela} (${columnsArray.join()}) VALUES(${flagsArray.join()}) RETURNING *`;
 
       await cliente.query("BEGIN");
       const result = await cliente.query(queryText, valuesArray);
       await cliente.query("COMMIT");
 
-      return result.rows[0];
+      return result.rows[0]; // ✅ retorna o cliente com ID
     } catch (error) {
-      await cliente.query("ROLLBACK");
+      await cliente.query("ROLLBACK"); // Se der algum problema, desfazer
       throw error;
     } finally {
-      cliente.release();
+      cliente.release(); // largar a conexão com banco de dados
     }
   }
 
