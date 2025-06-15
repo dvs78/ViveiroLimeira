ba0dc09f2891b79be7a2b5824f4c62daba6d1f8c
Daniel Veiga <dvs.veiga78@gmail.com>
2025-06-15 16:55:53 -0300

    Tentando 3 ainda fazer o deploy

--- back-end/apiBack/connect.js
+++ back-end/apiBack/connect.js
@@ -1,13 +1,24 @@
-import pg from "pg";
+// import pg from "pg";
 
-const pool = new pg.Pool({
-  host: "localhost",
-  port: 5432,
-  user: "postgres",
-  password: "Limao_10",
-  database: "ViveiroLimeira",
-});
+// const pool = new pg.Pool({
+//   host: "localhost",
+//   port: 5432,
+//   user: "postgres",
+//   password: "Limao_10",
+//   database: "ViveiroLimeira",
+// });
 
-export default pool;
+// export default pool;
 
 // console.log((await pool.query("SELECT * FROM clientes")).rows);
+
+import pg from "pg";
+import "dotenv/config";
+const pool = new pg.Pool({
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_DATABASE,
+});
+export default pool;
